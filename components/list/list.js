c1d058abda1dd98d18c2d876a06bc8d3c6bca9d5
顾文庆 <guwenqing@itimebox.com>
2020-06-03 23:04:48 +0800

    xiugai

--- components/list/list.js
+++ components/list/list.js
@@ -4,20 +4,25 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    list:{
+      type:Array
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    
+  },
+  attached(){
+    console.log("1111",this.properties.list)
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
     navTab: ['全部','精华','分享', '客户端测试'],        
     currentTab: 0,
     sendList:[],
+    list:[]
   },
   select: {
     page: 1,
@@ -15,7 +16,7 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
-    
+    this.getInitData()
   },
   currentTab: function (e) {
     if (this.data.currentTab == e.currentTarget.dataset.idx){
@@ -31,4 +32,22 @@ Page({
     }
     this.data.sendList=[];
   },
+  getInitData(){
+      wx.request({
+        url:"https://cnodejs.org/api/v1/topics",
+        header: {
+        'content-type': 'application/json' // 默认值
+        },
+        data:{
+
+        },
+        success:res => {
+          if (res.statusCode == 200){
+            this.setData({
+              list:res.data.data
+            })
+          }
+        }
+      })
+    },
 })
