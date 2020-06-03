27a2d5a8ebd3e97aba97038a151975a2ac1f84c2
顾文庆 <guwenqing@itimebox.com>
2020-06-03 18:24:20 +0800

    第一次接触小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    navTab: ['全部','精华','分享', '客户端测试'],        
+    currentTab: 0,
+    sendList:[],
+  },
+  select: {
+    page: 1,
+    size: 6,
+    isEnd: false
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  currentTab: function (e) {
+    if (this.data.currentTab == e.currentTarget.dataset.idx){
+      return;
+    }
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      currentTab: e.currentTarget.dataset.idx
     })
-  }
+    this.select={
+      page: 1,
+      size: 6,
+      isEnd: false
+    }
+    this.data.sendList=[];
+  },
 })
