691d856329c43437e4be096b290ae804b4c72041
顾文庆 <guwenqing@itimebox.com>
2020-06-22 10:34:00 +0800

    修改样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,21 +15,16 @@ Page({
     sendList:[],
     list:[],
   },
-  select: {
-    page: 1,
-    size: 6,
-    isEnd: false
-  },
   //事件处理函数
   onLoad: function () {
     this.getData()
   },
   currentTab: function (e) {
-    console.log("e",e.currentTarget)
     this.getData({tab:e.currentTarget.dataset.name})
     this.setData({
       currentTab: e.currentTarget.dataset.id
     })
+    console.log("e",e.currentTarget.dataset.id == this.data.currentTab)
   },
   getData(data = {}){
     request("topics",'get',data).then(res => {
